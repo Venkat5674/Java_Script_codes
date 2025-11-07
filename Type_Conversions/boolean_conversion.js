b3cb83e5ffb119bcdcb8fc197749c0381e105b8a
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-11-07 21:00:26 +0530

    Create boolean_conversion.js

---
+++ Type_Conversions/boolean_conversion.js
@@ -0,0 +1,15 @@
+// boolean_conversion.js
+
+// Truthy values → true
+console.log(Boolean(1));        // true
+console.log(Boolean("Hello"));  // true
+console.log(Boolean(5.5));      // true
+console.log(Boolean([]));       // true
+console.log(Boolean({}));       // true
+
+// Falsy values → false
+console.log(Boolean(0));        // false
+console.log(Boolean(""));       // false
+console.log(Boolean(null));     // false
+console.log(Boolean(undefined));// false
+console.log(Boolean(NaN));      // false
