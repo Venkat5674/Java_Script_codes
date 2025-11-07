eced0272a5c68c57d461b7252f3f7e22c36c43a4
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-11-07 21:01:25 +0530

    Create implicit_conversion.js

---
+++ Type_Conversions/implicit_conversion.js
@@ -0,0 +1,13 @@
+// implicit_conversion.js
+
+// Number + String → String
+console.log(5 + "5");  // "55"
+
+// String converted to Number for math operations
+console.log("10" - 2); // 8
+console.log("6" * 2);  // 12
+console.log("9" / 3);  // 3
+
+// Boolean converted to Number
+console.log(true + 1); // 2
+console.log(false + 5);// 5
