4bf786ce924d0ad5718a827bbbcf58ac2dfae277
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-11-17 22:58:47 +0530

    Create script.js

---
+++ DOM/script.js
@@ -0,0 +1,10 @@
+function fun(){
+    let abcElem=document.getElementById("login_text");
+    // abcElem.style.backgroundImage=""
+    abcElem.innerText="Login Successfull";
+    abcElem.style.backgroundColor="green";
+    abcElem.style.color="white";
+    abcElem.style.border="2px solid black";
+    abcElem.style.width="270px";
+     
+}
