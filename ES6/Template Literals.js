a97432a7b30e3930eeee9ca9a636f6d1f3b868d0
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-11-25 22:15:34 +0530

    Create Template Literals.js

---
+++ ES6/Template Literals.js
@@ -0,0 +1,13 @@
+const name = "Venkatesh";
+const age = 22;
+
+const msg = `My name is ${name} and I am ${age} years old.`;
+console.log(msg);
+
+// Multi-line
+const multi = `
+Line 1
+Line 2
+Line 3
+`;
+console.log(multi);
