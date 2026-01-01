9b045275ff2712c860ced0b0cd5819fabf2b0849
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-01 11:16:03 +0530

    Create Day1.js

---
+++ Basics/Day1.js
@@ -0,0 +1,18 @@
+// Print "Hello JavaScript"
+console.log("Hello JavaScript");
+
+// Declare two numbers and print their sum
+let a = 20;
+let b = 21;
+console.log(a + b);
+
+// Store name in a variable and print it
+let name = "Venkatesh Pamudurti";
+console.log(name);
+
+// Declare a const value
+const pi = 3.14;
+console.log(pi);
+
+// Trying to change const value (will cause error if uncommented)
+// pi = 4.12;
