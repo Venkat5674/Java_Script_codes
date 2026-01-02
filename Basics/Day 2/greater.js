2b44ed15b3f576078395e896b1bb1b2343154ffb
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-02 22:43:19 +0530

    Create greater.js

---
+++ Basics/Day 2/greater.js
@@ -0,0 +1,8 @@
+let a = prompt("Enter first number:");
+let b = prompt("Enter second number:");
+
+if (a > b) {
+    console.log(a + " is greater");
+} else {
+    console.log(b + " is greater");
+}
